2e9f9ff285995731b8b318fbf89a97a267302013
ahmed <ahmedatabg@gmail.com>
2021-04-04 23:00:43 +0100

    SPLASH SCREEN + VIEW CLEAN 2

--- app/src/main/java/com/example/multipurposeapp/SplashActivity.java
+++ app/src/main/java/com/example/multipurposeapp/SplashActivity.java
@@ -21,10 +21,10 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashActivity.this,LoginActivity.class);
-                SplashActivity.this.startActivity(mainIntent);
-                SplashActivity.this.finish();
+
+                Intent loginIntent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
